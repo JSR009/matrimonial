b3d8271d4fa45d1ad432a7842901880181b4a7cf
harshvarddhan2087 <harshvardhanyadav2087@gamil.com>
2024-10-05 15:04:19 +0530

    added the env file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAaBEPKg4V4YVhyBoyzUUD8C6JoVYz-gIA",
-    authDomain: "matrimonial-c5bf1.firebaseapp.com",
-    projectId: "matrimonial-c5bf1",
-    storageBucket: "matrimonial-c5bf1.appspot.com",
-    messagingSenderId: "1080183363787",
-    appId: "1:1080183363787:web:cd1da5c514479e14cc634e",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
