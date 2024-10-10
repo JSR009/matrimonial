fc7e537e51881a1ba3b852df68bb24e6ae3f9453
JSR009 <realjsrgroup@gmail.com>
2024-10-10 23:03:05 +0530

    fixing the bug

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,5 @@
 import { Metadata } from "next";
 import Hero from "@/components/Hero";
-import Brands from "@/components/Brands";
-import Feature from "@/components/Features";
 import About from "@/components/About";
 import FeaturesTab from "@/components/FeaturesTab";
 import FunFact from "@/components/FunFact";
@@ -10,7 +8,6 @@ import CTA from "@/components/CTA";
 import FAQ from "@/components/FAQ";
 import Pricing from "@/components/Pricing";
 import Contact from "@/components/Contact";
-import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 import Header from "@/components/Header";
 
@@ -25,8 +22,6 @@ export default function Home() {
     <main>
       <Header />
       <Hero />
-      {/* <Brands /> */}
-      {/* <Feature /> */}
       <About />
       <FeaturesTab />
       <FunFact />
@@ -36,7 +31,6 @@ export default function Home() {
       <Testimonial />
       <Pricing />
       <Contact />
-      {/* <Blog /> */}
     </main>
   );
 }
