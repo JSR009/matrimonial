db947be8d5b231c2a59f019b41ba99c419f3a6af
JSR009 <realjsrgroup@gmail.com>
2024-11-12 17:07:51 +0530

    making touch up in the ui

--- app/(site)/profile/page.tsx
+++ app/(site)/profile/page.tsx
@@ -153,73 +153,93 @@ const Profile = () => {
 
   return (
     <div className="container mx-auto px-4 py-10 space-y-8 mt-8">
-      <div className="bg-gradient-to-r from-purple-400 to-blue-500 p-8 rounded-lg shadow-lg text-white flex items-center justify-center flex-col">
-        <div className="relative w-40 h-40 mb-4">
-          <Image
-            src={user.profileImage || user.portfolioImages[0] || "/images/default-profile.png"}
-            alt="Profile"
-            className="rounded-full w-full h-full object-cover border-4 border-white shadow-lg"
-            width={100}
-            height={100}
-          />
-          <label className="absolute bottom-2 right-2 p-1 bg-white text-gray-700 rounded-full cursor-pointer">
-            <FaCamera />
-            <input type="file" onChange={(e) => e.target.files && handleImageUpload(e.target.files[0], `profileImages/${user.email}`)} hidden />
-          </label>
-        </div>
-        <h2 className="text-3xl font-semibold">{user.name || "No name provided"}</h2>
-        <p>{user.email || "No email provided"}</p>
-        <button onClick={handleEditToggle} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md shadow-md flex items-center space-x-2">
-          <FaEdit />
-          <span>Edit Profile</span>
-        </button>
-      </div>
+  <div className="bg-gradient-to-r from-purple-400 to-blue-500 p-8 rounded-lg shadow-lg text-white flex items-center justify-center flex-col">
+    <div className="relative w-40 h-40 mb-4">
+      <Image
+        src={user.profileImage || user.portfolioImages[0] || "/images/default-profile.png"}
+        alt="Profile"
+        className="rounded-full w-full h-full object-cover border-4 border-white shadow-lg"
+        width={100}
+        height={100}
+      />
+      <label className="absolute bottom-2 right-2 p-1 bg-white text-gray-700 rounded-full cursor-pointer">
+        <FaCamera />
+        <input
+          type="file"
+          onChange={(e) => e.target.files && handleImageUpload(e.target.files[0], `profileImages/${user.email}`)}
+          hidden
+        />
+      </label>
+    </div>
+    <h2 className="text-3xl font-semibold">{user.name || "No name provided"}</h2>
+    <p>{user.email || "No email provided"}</p>
+    <button
+      onClick={handleEditToggle}
+      className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md shadow-md flex items-center space-x-2"
+    >
+      <FaEdit />
+      <span>Edit Profile</span>
+    </button>
+  </div>
 
-      {updateMessage && <div className="bg-green-500 text-white text-center p-4 rounded-md">{updateMessage}</div>}
+  {updateMessage && (
+    <div className="bg-green-500 text-white text-center p-4 rounded-md">
+      {updateMessage}
+    </div>
+  )}
 
-      {isEditing && (
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg h-3/4 overflow-y-auto">
-            <h2 className="text-xl font-semibold mb-4">Edit Profile</h2>
-            {profileFields.map(({ label, key }) => (
-              <div key={key} className="mb-4">
-                <label className="block text-gray-700">{label}</label>
-                <input
-                  type="text"
-                  value={(editData[key] as string) || ""}
-                  onChange={(e) => handleInputChange(key, e.target.value)}
-                  className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-                />
-              </div>
-            ))}
-            <div className="flex justify-end space-x-4 mt-4">
-              <button onClick={handleEditToggle} className="px-4 py-2 bg-gray-400 text-white rounded-md">
-                Cancel
-              </button>
-              <button onClick={handleProfileUpdate} className="px-4 py-2 bg-blue-600 text-white rounded-md">
-                Save Changes
-              </button>
+  {isEditing && (
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50 px-4">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg h-3/4 overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4 text-center">Edit Profile</h2>
+        <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {profileFields.map(({ label, key }) => (
+            <div key={key} className="w-full">
+              <label className="block text-gray-700 text-sm font-medium mb-1">
+                {label}
+              </label>
+              <input
+                type="text"
+                value={(editData[key] as string) || ""}
+                onChange={(e) => handleInputChange(key, e.target.value)}
+                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+              />
             </div>
-          </div>
+          ))}
         </div>
-      )}
 
-      <div className="bg-white p-6 rounded-lg shadow-lg space-y-4">
-        <h2 className="text-2xl font-semibold">Profile Details</h2>
-        <div className="grid grid-cols-2 gap-4">
-        {profileFields.map(({ label, key }) => (
-          <div key={key} className="flex justify-between bg-gray-100 p-3 rounded-md">
-            <span className="font-medium text-gray-700">{label}:</span>
-            <span className="text-gray-900">
-              {user[key] instanceof Date
-                ? user[key].toLocaleDateString() // Format the Date if it's a Date object
-                : user[key] !== undefined
-                ? String(user[key]) // Convert to string if it's not undefined
-                : "Not provided"}
-            </span>
-          </div>
-        ))}
+        <div className="flex justify-end space-x-4 mt-4">
+          <button onClick={handleEditToggle} className="px-4 py-2 bg-gray-400 text-white rounded-md">
+            Cancel
+          </button>
+          <button onClick={handleProfileUpdate} className="px-4 py-2 bg-blue-600 text-white rounded-md">
+            Save Changes
+          </button>
         </div>
+      </div>
+    </div>
+  )}
+
+  <div className="bg-white p-6 rounded-lg shadow-lg space-y-4">
+    <h2 className="text-2xl font-semibold">Profile Details</h2>
+    <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2">
+      {profileFields.map(({ label, key }) => (
+        <div key={key} className="flex justify-between items-center bg-gray-100 p-3 rounded-md">
+          <span className="font-medium text-gray-700">{label}:</span>
+          <span className="text-gray-900">
+            {user[key] instanceof Date
+              ? user[key].toLocaleDateString()
+              : user[key] !== undefined
+              ? String(user[key])
+              : "Not provided"}
+          </span>
+        </div>
+      ))}
+    </div>
+  
+
+
+        
         <div className="bg-white p-6 rounded-lg shadow-lg space-y-4">
         <h2 className="text-2xl font-semibold">Portfolio Images</h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -44,12 +44,14 @@ const Hero = () => {
                   My Profile
                 </button>
               </Link>
+              <Link href="/">
               <button
                 onClick={handleLogout}
                 className="bg-red-500 text-white px-6 py-3 rounded-lg hover:bg-red-600 transition"
               >
                 Logout
               </button>
+              </Link>
             </>
           ) : (
             <>
