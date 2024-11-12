997139bd302197e413fe2911ced769b3b5ba244f
JSR009 <realjsrgroup@gmail.com>
2024-11-12 16:13:57 +0530

    changes in the adminlogin file

--- app/(site)/admin/page.tsx
+++ app/(site)/admin/page.tsx
@@ -1,10 +1,13 @@
+"use client";
 import React from 'react'
 import AdminLogin from '@/components/AdminLogin'
 
 const page = () => {
   return (
     <div>
-      <AdminLogin />
+      <AdminLogin onLogin={function (): void {
+        throw new Error('Function not implemented.')
+      } } />
       
     </div>
   )
